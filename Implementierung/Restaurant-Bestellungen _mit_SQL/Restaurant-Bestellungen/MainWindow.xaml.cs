4f9fb444b71b8bc4edd043f7516f751448acbd63
Francesco <francesco@outlook.de>
2022-01-30 14:32:35 +0100

    added cosmetics und Änderungen an Datagrids

--- Implementierung/Restaurant-Bestellungen _mit_SQL/Restaurant-Bestellungen/MainWindow.xaml.cs
+++ Implementierung/Restaurant-Bestellungen _mit_SQL/Restaurant-Bestellungen/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace Restaurant_Bestellungen
     {
         List<Rechnung_element> Rechnungspositionen;
         private Restaurant_BestellungenEntities ctx = new Restaurant_BestellungenEntities();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -38,6 +39,19 @@ namespace Restaurant_Bestellungen
             MainGrid_Rechnungsübersicht.Visibility = Visibility.Hidden;
         }
 
+        private void Button_RechnungErstellen(object sender, RoutedEventArgs e)
+        {
+            MainGrid_Menü.Visibility = Visibility.Hidden;
+            //...
+        }
+        private void Button_Github(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/Gruppe4-Restaurant/Restaurant-Bestellungen");
+        }
+
+
+
+
         private void Button_SpeisekarteVerwalten(object sender, RoutedEventArgs e)
         {
             MainGrid_Menü.Visibility = Visibility.Hidden;
@@ -48,7 +62,7 @@ namespace Restaurant_Bestellungen
 
         private void Button_DeleteCurrentItem(object sender, RoutedEventArgs e)
         {
-            int id = (int)AusgewähltesProdukt_ID.Content;
+            int id = (int)AusgewähltesProdukt_ID.Content; //Label (hidden)
             Speise s = ctx.Speise.Where(x => x.Produkt_ID == id).FirstOrDefault();
             ctx.Speise.Remove(s);
             Speichern();
@@ -63,7 +77,9 @@ namespace Restaurant_Bestellungen
                 NeueSpeise.Produkt_Name = AddName.Text;
                 //ctx.Speise.Count();
                 ctx.Speise.Add(NeueSpeise);
-
+                AddBeschreibung.Text = "";
+                AddName.Text = "";
+                AddPreis.Text = "";
             }
             Speichern();
         }
@@ -75,31 +91,33 @@ namespace Restaurant_Bestellungen
             MainGrid_SpeisekarteVerwalten.DataContext = ctx.Speise.ToList();
         }
 
-        private void Button_RechnungErstellen(object sender, RoutedEventArgs e)
-        {
-            MainGrid_Menü.Visibility = Visibility.Hidden;
-            //...
-        }
+       
         private void Button_Rechnungsübersicht(object sender, RoutedEventArgs e)
         {
             MainGrid_Menü.Visibility = Visibility.Hidden;
             MainGrid_Rechnungsübersicht.Visibility = Visibility.Visible;
             ctx.Rechnung.Load();
-            Grid_Rechnungen.DataContext = ctx.Rechnung.ToList();
-           
+            Grid_Rechnungen.DataContext = ctx.Rechnung.ToList();      
             //tb.Text = r.Rechnung_element.Count().ToString();
-        }
-
-        private void Button_Github(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://github.com/Gruppe4-Restaurant/Restaurant-Bestellungen");
         }
 
         private void Grid_Rechnungen_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            decimal total = 0;
             Rechnung r = (Rechnung)Grid_Rechnungen.SelectedItem;
             Rechnungspositionen = ctx.Rechnung_element.Where(x => x.Rechnung_id == r.Rechnung_id).ToList();
             Grid_Rechnungselemente.DataContext = Rechnungspositionen.ToList();
+
+            foreach(Rechnung_element pos in Rechnungspositionen)
+            {
+                total += pos.Speise.Preis * pos.Anzahl;
+            }
+            Total.Text = Convert.ToString(total);
+        }
+
+        private void Button_Status_ändern_Rechnungen(object sender, RoutedEventArgs e)
+        {
+            ctx.SaveChanges();
         }
     }
 }
